20d8d44575e7ecf7fb21ccf7e5fead033c7be09e
Egorov Nikita Andreevich <egorovna.manager@mail.ru>
2024-12-01 10:42:24 +0700

    add notification channel

--- app/src/main/java/egorovna/streamic/App.java
+++ app/src/main/java/egorovna/streamic/App.java
@@ -1,6 +1,21 @@
 package egorovna.streamic;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        String name = getString(R.string.notification_channel_name);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel channel = new NotificationChannel(name, name, importance);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
 }
