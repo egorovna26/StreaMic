1408d84e7ffa2619d811f02e1c323da11f5e9a1a
Egorov Nikita Andreevich <egorovna.manager@mail.ru>
2024-12-01 11:26:47 +0700

    add request permissions

--- app/src/main/java/egorovna/streamic/activity/MainActivity.java
+++ app/src/main/java/egorovna/streamic/activity/MainActivity.java
@@ -1,18 +1,67 @@
 package egorovna.streamic.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import egorovna.streamic.R;
 
 public class MainActivity extends AppCompatActivity {
+    private CardView postNotificationsCard;
+    private CardView recordAudioCard;
+    private Button allowPostNotifications;
+    private Button allowRecordAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        postNotificationsCard = findViewById(R.id.post_notifications_card);
+        recordAudioCard = findViewById(R.id.record_audio_card);
+        allowPostNotifications = findViewById(R.id.allow_post_notifications);
+        allowRecordAudio = findViewById(R.id.allow_record_audio);
+        checkPermissions();
+        allowPostNotifications.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+            @Override
+            public void onClick(View v) {
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+            }
+        });
+        allowRecordAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            postNotificationsCard.setVisibility(View.GONE);
+        } else {
+            postNotificationsCard.setVisibility(View.VISIBLE);
+        }
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            recordAudioCard.setVisibility(View.GONE);
+        } else {
+            recordAudioCard.setVisibility(View.VISIBLE);
+        }
     }
 }
