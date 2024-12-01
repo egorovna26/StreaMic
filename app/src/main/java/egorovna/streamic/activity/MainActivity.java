6bdac793987e369587537d8e047d9b9072047e7d
Egorov Nikita Andreevich <egorovna.manager@mail.ru>
2024-12-01 11:11:50 +0700

    add permission cards

--- app/src/main/java/egorovna/streamic/activity/MainActivity.java
+++ app/src/main/java/egorovna/streamic/activity/MainActivity.java
@@ -1,13 +1,9 @@
 package egorovna.streamic.activity;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.text.MessageFormat;
 
 import egorovna.streamic.R;
 
@@ -18,7 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        TextView device = findViewById(R.id.device);
-        device.setText(MessageFormat.format("{0} {1}", Build.MANUFACTURER, Build.MODEL));
     }
 }
