b7b5b376fafae955c6bc02174ae43d78ed719ab3
Egorov Nikita Andreevich <egorovna.manager@mail.ru>
2024-12-03 23:56:04 +0700

    add websocket server

--- app/src/main/java/egorovna/streamic/server/AudioServer.java
+++ app/src/main/java/egorovna/streamic/server/AudioServer.java
@@ -1,8 +1,16 @@
 package egorovna.streamic.server;
 
+import android.annotation.SuppressLint;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.util.Log;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +20,6 @@ import lombok.Setter;
 public class AudioServer implements Runnable {
     private static final int SAMPLE_RATE = 44100;
     private static final int AUDIO_SOURCE = MediaRecorder.AudioSource.MIC;
-    private static final int RAW_AUDIO_SOURCE = MediaRecorder.AudioSource.UNPROCESSED;
     private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_STEREO;
     private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
     private static final int MIN_BUFFER_SIZE = AudioRecord.getMinBufferSize(
@@ -22,26 +29,62 @@ public class AudioServer implements Runnable {
     );
 
     private boolean working = false;
+    private WebSocketServer webSocketServer;
     private AudioRecord audioRecord;
 
+    @SuppressLint("MissingPermission")
     @Override
     public void run() {
+        audioRecord = new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, MIN_BUFFER_SIZE);
         if (audioRecord.getState() != AudioRecord.STATE_INITIALIZED) {
             return;
         }
         audioRecord.startRecording();
+        webSocketServer = new WebSocketServer(new InetSocketAddress(8888)) {
+            @Override
+            public void onOpen(WebSocket conn, ClientHandshake handshake) {
+
+            }
+
+            @Override
+            public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+
+            }
+
+            @Override
+            public void onMessage(WebSocket conn, String message) {
+
+            }
+
+            @Override
+            public void onError(WebSocket conn, Exception ex) {
+
+            }
+
+            @Override
+            public void onStart() {
+
+            }
+        };
+        webSocketServer.setReuseAddr(true);
+        webSocketServer.start();
         byte[] buffer = new byte[MIN_BUFFER_SIZE];
+        setWorking(true);
         while (working) {
             audioRecord.read(buffer, 0, buffer.length);
+            webSocketServer.broadcast(buffer);
         }
         audioRecord.stop();
         audioRecord.release();
-        setAudioRecord(null);
+        audioRecord = null;
+        try {
+            webSocketServer.stop();
+        } catch (Exception e) {
+            Log.e(null, null, e);
+        }
     }
 
     public void startServer() {
-        setWorking(true);
-        setAudioRecord(new AudioRecord(AUDIO_SOURCE, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, MIN_BUFFER_SIZE));
         Thread thread = new Thread(this);
         thread.start();
     }
